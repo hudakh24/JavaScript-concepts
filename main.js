ac282f9ec731c73365059953ec078bbb5f4ac9a6
hudakh24 <hudashoaib2000@gmail.com>
2024-07-10 13:04:19 +0500

    changes done

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
 const secondary = () => {
     const a = 2;
     console.log("secondary");
   };
-=======
 const primary = () => {
     console.log("primary");
->>>>>>> function/primary
   };
 
 const main = () => {
     console.log("main function");
   };
-<<<<<<< HEAD
-  
-=======
     
->>>>>>> function/primary
   const palidrome = () => {
     console.log("palidrome");
   };
