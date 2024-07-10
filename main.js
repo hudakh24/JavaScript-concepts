2bcf59d5e1895f626eecb8b4913e3b02cfb82c82
hudakh24 <hudashoaib2000@gmail.com>
2024-07-10 12:17:39 +0500

    primary filr created

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
+const primary = () => {
+    console.log("primary");
+  };
+
 const main = () => {
     console.log("main function");
   };
-  
-  const evenOdd = () => {
-    const a = 2;
-    a % 2 == 0 ? console.log("even") : console.log("odd");
-  };
-
+    
   const palidrome = () => {
     console.log("palidrome");
   };
+  
   
