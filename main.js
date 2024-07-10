4e6fb307fcb2ba538d5c36678c14c4ae95260608
hudakh24 <hudashoaib2000@gmail.com>
2024-07-10 12:27:56 +0500

    secondary file  created

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+const evenOdd = () => {
+    const a = 2;
+    a % 2 == 0 ? console.log("even") : console.log("odd");
+  };
+  
 const main = () => {
     console.log("main function");
   };
   
-  const evenOdd = () => {
-    const a = 2;
-    a % 2 == 0 ? console.log("even") : console.log("odd");
-  };
-
   const palidrome = () => {
     console.log("palidrome");
   };
