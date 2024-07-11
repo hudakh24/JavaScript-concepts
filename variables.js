39fa9688b32456b19abb4bcf539f697dd3ff1fa5
hudakh24 <hudashoaib2000@gmail.com>
2024-07-11 13:46:04 +0500

    07:11:2024 Mern19

--- variables.js
+++ variables.js
@@ -4,8 +4,11 @@ const a = 1;
 console.log("a before scope", a);
 
 {
-  console.log("a in scope", a);
   const a = 1;
+  console.log("a in scope", a);
+
+  a = "new value";
+  console.log("a in scope after update", a);
 }
 
 console.log("a after scope", a);
