60762013b40b05b4b232c02d580ac290de37f80d
hudakh24 <hudashoaib2000@gmail.com>
2024-07-11 13:33:12 +0500

    07:11:2024

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 const secondary = () => {
-    const a = 2;
-    console.log("secondary");
-  };
+  const a = 2;
+  console.log("secondary");
+};
 const primary = () => {
-    console.log("primary");
-  };
+  console.log("primary");
+};
 
 const main = () => {
-    console.log("main function");
-  };
-    
-  const palidrome = () => {
-    console.log("palidrome");
-  };
-  
-  
+  console.log("main function");
+};
+
+const palidrome = () => {
+  console.log("palidrome");
+};
