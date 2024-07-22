f1dc555c9f8cd7cd662dc714266556b32a19efe5
hudakh24 <hudashoaib2000@gmail.com>
2024-07-22 13:21:05 +0500

    07:22:2023

---
+++ operators.js
@@ -0,0 +1,36 @@
+// Arthematic operators
+const a = 10;
+const b = 3;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulus
+console.log(a ** b); // Exponentiation
+
+//Comparison operators
+console.log(a == b); // Equal to
+console.log(a != b); // Not equal to
+console.log(a === b); // Strict equal to
+console.log(a !== b); // Strict not equal to
+console.log(a > b); // Greater than
+console.log(a < b); // Less than
+console.log(a >= b); // Greater than or equal to
+console.log(a <= b); // Less than or equal to
+
+//Logical Operators
+let x = true;
+let y = false;
+console.log(x && y); // Logical AND
+console.log(x || y); // Logical OR
+console.log(!x); // Logical NOT
+
+//Bitwise Operators
+console.log(a & b); // Bitwise AND
+console.log(a | b); // Bitwise OR
+console.log(a ^ b); // Bitwise XOR
+console.log(~a); // Bitwise NOT
+console.log(a << 1); // Bitwise left shift
+console.log(a >> 1); // Bitwise right shift
+console.log(a >>> 1); // Bitwise unsigned right shift
