02ad6e5a8b8d0d808c42bbdc4e19276d150cf9e5
hudakh24 <hudashoaib2000@gmail.com>
2024-07-22 13:47:39 +0500

    07:22:2023

---
+++ extra.js
@@ -0,0 +1,14 @@
+//type conversion
+
+let str = "123.45"; //string to number
+let intNum = parseInt(str);
+let floatNum = parseFloat(str);
+console.log(intNum, floatNum);
+
+let num = 123;
+let str1 = String(num); // Converts number  to string
+console.log(str1);
+
+let bool = true;
+let number1 = Number(bool); // Converts boolean to number
+console.log(number1); // 1
