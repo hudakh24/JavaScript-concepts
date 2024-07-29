94b4fc0c44d4eef7ba68ebe7b89a785b9fbfca02
hudakh24 <hudashoaib2000@gmail.com>
2024-07-29 12:44:28 +0500

    07:29:24

---
+++ userController.js
@@ -0,0 +1,19 @@
+const userController = {
+  createUser: () => {
+    console.log("create user function");
+  },
+
+  getUser: () => {
+    console.log("get user function");
+  },
+
+  updateUser: () => {
+    console.log("update user function");
+  },
+
+  deleteUser: () => {
+    console.log("delete user function");
+  },
+};
+
+module.exports = userController;
